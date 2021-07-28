39c43eaaec28c1dad5a2dfd6bcb0df4efdf55256
JakeHildy <jacob.hildebrandt@gmail.com>
2021-07-27 17:12:12 -0700

    Adoption contract written and tests setup

--- src/js/app.js
+++ src/js/app.js
@@ -2,19 +2,19 @@ App = {
   web3Provider: null,
   contracts: {},
 
-  init: async function() {
+  init: async function () {
     // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
+    $.getJSON("../pets.json", function (data) {
+      var petsRow = $("#petsRow");
+      var petTemplate = $("#petTemplate");
 
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+      for (i = 0; i < data.length; i++) {
+        petTemplate.find(".panel-title").text(data[i].name);
+        petTemplate.find("img").attr("src", data[i].picture);
+        petTemplate.find(".pet-breed").text(data[i].breed);
+        petTemplate.find(".pet-age").text(data[i].age);
+        petTemplate.find(".pet-location").text(data[i].location);
+        petTemplate.find(".btn-adopt").attr("data-id", data[i].id);
 
         petsRow.append(petTemplate.html());
       }
@@ -23,7 +23,7 @@ App = {
     return await App.initWeb3();
   },
 
-  initWeb3: async function() {
+  initWeb3: async function () {
     /*
      * Replace me...
      */
@@ -31,7 +31,7 @@ App = {
     return App.initContract();
   },
 
-  initContract: function() {
+  initContract: function () {
     /*
      * Replace me...
      */
@@ -39,30 +39,29 @@ App = {
     return App.bindEvents();
   },
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+  bindEvents: function () {
+    $(document).on("click", ".btn-adopt", App.handleAdopt);
   },
 
-  markAdopted: function() {
+  markAdopted: function () {
     /*
      * Replace me...
      */
   },
 
-  handleAdopt: function(event) {
+  handleAdopt: function (event) {
     event.preventDefault();
 
-    var petId = parseInt($(event.target).data('id'));
+    var petId = parseInt($(event.target).data("id"));
 
     /*
      * Replace me...
      */
-  }
-
+  },
 };
 
-$(function() {
-  $(window).load(function() {
+$(function () {
+  $(window).load(function () {
     App.init();
   });
 });

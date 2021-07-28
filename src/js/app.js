9c9c9658a30c1c6c9e0897ac3b89131ef446e39f
JakeHildy <jacob.hildebrandt@gmail.com>
2021-07-28 08:59:20 -0700

    Initializing web3

--- src/js/app.js
+++ src/js/app.js
@@ -25,9 +25,30 @@ App = {
 
   initWeb3: async function () {
     console.log("initWeb3");
-    /*
-     * Replace me...
-     */
+
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+        // Request account access
+        await window.ethereum.enable();
+      } catch (error) {
+        // User denied account access...
+        console.error("User denied account access");
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    }
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+      App.web3Provider = new Web3.providers.HttpProvider(
+        "http://localhost:7545"
+      );
+    }
+    web3 = new Web3(App.web3Provider);
+    console.log(web3);
 
     return App.initContract();
   },
@@ -56,7 +77,7 @@ App = {
     event.preventDefault();
 
     var petId = parseInt($(event.target).data("id"));
-
+    console.log("handle adopt pet: ", petId);
     /*
      * Replace me...
      */
